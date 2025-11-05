3b1c8debc9a88df84d63434be7a92aa54013a105
Onkar Patil <onkar092005@gmail.com>
2025-11-05 14:57:18 +0530

    Connected db

--- backend/database.py
+++ backend/database.py
@@ -1,19 +1,21 @@
+# database.py
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
-load_dotenv() #it loads .env variables
+load_dotenv()
 
-DB_HOST=os.getenv(DB_HOST)
-DB_NAME=os.getenv(DB_NAME)
-DB_USER=os.getenv(DB_USER)
-DB_PORT=os.getenv(DB_PORT)
-DB_PASSWORD=os.getenv(DB_PASSWORD)
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
+
 
 database_url=f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine=create_engine()
+engine=create_engine(database_url)
 sessionLocal=sessionmaker(bind=engine,autoflush=False,autocommit=False)
 
 Base=declarative_base()
