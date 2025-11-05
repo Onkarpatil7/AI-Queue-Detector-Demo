5dbbd2f7b5f3ff0f36cd771ea1779774a86d24b9
Onkar Patil <onkar092005@gmail.com>
2025-11-05 18:23:19 +0530

    structered and created table in postgreSQL

--- backend/models.py
+++ backend/models.py
@@ -5,8 +5,7 @@ from datetime import datetime
 class queueData(Base):
     __tablename__="queuedata"
 
-
+    id=Column(Integer,primary_key=True,index=True)
     timeStamp=Column(DateTime,default=datetime.now,primary_key=True)
-    peopleCount=Column(Integer)
-    waitTime=Column(Integer)
+    waitTime=Column(Float)
 
